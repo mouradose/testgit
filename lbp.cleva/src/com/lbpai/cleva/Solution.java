0728af34f91910722149187898d664f8507bfbee
xyao432 <xyao432@752474SS262.dct.adt.local>
2017-05-24 14:51:06 +0200

    test4

--- lbp.cleva/src/com/lbpai/cleva/Solution.java
+++ lbp.cleva/src/com/lbpai/cleva/Solution.java
@@ -3,7 +3,7 @@ package com.lbpai.cleva;
 public class Solution {
 
 	public static void main(String[] args) {
-		/// TODO Auto-generated method stub.
+		/// 4 TODO Auto-generated method stub.
 		System.out.println("begin");
 	}
 
